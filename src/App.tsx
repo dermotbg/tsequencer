62591e31dc82a5b9bf88c83c3b79f616f9a13a2d
dermotbg <dermotbateman@gmail.com>
2024-04-11 16:53:15 +0300

    day 1 web audio api sequencer  attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,189 @@
 import { useState } from 'react'
 import './App.css'
+import { audioContext } from './utils/audioContext'
+import useInstruments from './hooks/useInstruments'
+
+type availableInstruments = 'kick' | 'clap' | 'closedHH' | ''
+
+interface StepSeqProps {
+  index: number
+  extraCSS: string
+  activePad: availableInstruments
+  step: Step
+}
+
+interface QueueSteps { step: number, time: number }
+
+type Step = {
+  instruments: [availableInstruments],
+  extraCSS: string
+}
+type Sequencer = Step[]
+
+
+const playSample = (audioContext: AudioContext, audioBuffer: AudioBuffer, time: number) => {
+  const sampleSource = new AudioBufferSourceNode(audioContext, {
+    buffer: audioBuffer
+    // possible to have playbackRate here if wanted in future (possible pitch-like adjustment?)
+  })
+  sampleSource.connect(audioContext.destination)
+  sampleSource.start(time)
+  return sampleSource
+}
+
+
+const StepSeqButton = ({index, extraCSS, activePad, step}: StepSeqProps) => {
+  const assignSampleHandler = () => {
+    console.log('inside step', activePad)
+    if(step.instruments.includes(activePad)){
+      console.log('splicestep', activePad)
+      step.instruments.splice(step.instruments.indexOf(activePad), 1)
+    }
+    else{
+      step.instruments.push(activePad)
+    }
+    console.log(step)
+  }
+  return (
+    <button 
+      className={'box-border border-4 p-4 rounded-md h-18 ' + `${extraCSS}`}
+      onClick={() => assignSampleHandler()} 
+      >
+      {index + 1}
+    </button>
+  )
+}
+
 
 
 function App() {
 
-  const [killswitch, setKillswitch] = useState<number | undefined>()
-  const [bpm, setBpm] = useState(140)
-
-  const kick = new Audio('https://cdn.freesound.org/previews/91/91627_1281492-lq.mp3')
-  const clap = new Audio('https://cdn.freesound.org/previews/561/561118_12517458-lq.mp3')
-  const closedHat = new Audio('https://cdn.freesound.org/previews/269/269720_4965320-lq.mp3')
-  const metroUp = new Audio('https://cdn.freesound.org/previews/250/250551_4570971-lq.mp3')
-  const metroDown = new Audio('https://cdn.freesound.org/previews/250/250552_4570971-lq.mp3')
-
-  const sequencer = (): void => {
-    let activePoint: number = 0
-      setKillswitch(
-        setInterval(() => {
-          if(activePoint === 0){
-            console.log('up')
-            metroUp.currentTime = 0
-            metroUp.play()
-          }
-    
-          else if(activePoint === 4 || activePoint === 8 || activePoint === 12) {
-            console.log('down')
-            metroDown.currentTime = 0
-            metroDown.play()
-          }
-          else{
-            console.log(activePoint)
-          }
-          activePoint++
-    
-          if(activePoint === 16) activePoint = 0
-        }, ((60 / bpm) * 1000) / 4)
-      ) 
+  const instruments = useInstruments()
+  const [bpm, setBpm] = useState<number>(120)
+  const [activePad, setActivePad] = useState<'kick' | 'clap' | 'closedHH' | ''>('')
+  // const [instruments, setInstruments]= useState<LoadedInstruments>()
+  const [seq] = useState<Sequencer>(Array.from({ length: 16 }, () => {
+    return {
+      instruments: [''],
+      extraCSS: ''
     }
+  }))
+  // const seq: Sequencer = Array.from({ length: 16 }, () => {
+  //   return {
+  //     instruments: [''],
+  //     extraCSS: ''
+  //   }
+  // })
 
-  const launchPad = (element: HTMLAudioElement) => {
-    element.currentTime = 0
-    element.play()
+  if(!instruments) return <>Loading...</>
+  if(!seq[0]) return <>Loading...</>
+
+  const lookahead = 25.0 // freq of scheduling function (miliseconds)
+  const scheduleAheadTime = 0.1 // how far ahead to schedule (seconds)
+  
+  let activeStep = 0
+  let nextStepTime = 0.0 // when next note is due
+  const nextStep = () => {
+    const secondsPerBeat = (60.0 / bpm) / 4
+    nextStepTime += secondsPerBeat
+    activeStep = (activeStep + 1) % 16
+  }
+  
+  const stepsInQueue: QueueSteps[] = []
+  const scheduleStep = async (stepNumber: number, time: number) => {
+    stepsInQueue.push({ step: stepNumber, time })
+    // if sampleArray[stepNumber] is assigned then play sample etc
+    if(seq[stepNumber].instruments.includes('kick')) {
+      playSample(audioContext, instruments.kick, time)
+    }
+    if(seq[stepNumber].instruments.includes('clap')) {
+      playSample(audioContext, instruments.clap, time)
+    }
+    if(seq[stepNumber].instruments.includes('closedHH')) {
+      playSample(audioContext, instruments.closedHH, time)
+    }
+  }
+  
+  
+  let timerId: ReturnType<typeof setInterval>
+  const scheduleSequencer = () => {
+    //advance the pointer while there are still steps to be played
+    while (nextStepTime < audioContext.currentTime + scheduleAheadTime) {
+      scheduleStep(activeStep, nextStepTime)
+      nextStep()
+    }
+    timerId = setTimeout(scheduleSequencer, lookahead)
+  }
+  
+  let lastStepHighlighted = 3
+  const colorSteps = () => {
+    let highlightStep = lastStepHighlighted
+    const currentTime = audioContext.currentTime
+    
+    while (stepsInQueue.length && stepsInQueue[0].time < currentTime) {
+      highlightStep = stepsInQueue[0].step
+      stepsInQueue.shift()
+    }
+    if(lastStepHighlighted !== highlightStep) {
+      seq.forEach(() => {
+        seq[lastStepHighlighted * 2].extraCSS = ''
+        seq[highlightStep * 2].extraCSS = 'border-lime-400'
+      })
+      lastStepHighlighted = highlightStep
+    }
+    requestAnimationFrame(colorSteps)
+  }
+  
+  let isPlaying = false
+  const launchSequencer = () => {
+    isPlaying = !isPlaying
+    if(isPlaying){
+      if(audioContext.state === 'suspended'){
+        audioContext.resume()
+      }
+      activeStep = 0
+      nextStepTime = audioContext.currentTime
+      scheduleSequencer()
+      window.requestAnimationFrame(colorSteps)
+    }
+    else {
+      clearTimeout(timerId)
+    }
+  }
+
+  const padHandler = (element: availableInstruments) => {
+    setActivePad(element)
+    playSample(audioContext, instruments[element], 0)
   }
 
   return (
     <>
       <button 
-        className="box-border h-32 w-32 border-4 rounded-md m-2"
-        onClick={() => launchPad(kick)}
+        className={activePad === 'kick' ? "box-border h-32 w-32 border-4 rounded-md m-2 border-lime-400" : "box-border h-32 w-32 border-4 rounded-md m-2"}
+        onClick={() => padHandler('kick') }
         >
         Kick
       </button>
       <button 
-        className="box-border h-32 w-32 border-4 rounded-md m-2"
-        onClick={() => launchPad(clap)}
+        className={activePad === 'clap' ? "box-border h-32 w-32 border-4 rounded-md m-2 border-blue-400" : "box-border h-32 w-32 border-4 rounded-md m-2"}
+        onClick={() => padHandler('clap')}
         >
         Clap
       </button>
       <button 
-        className="box-border h-32 w-32 border-4 rounded-md m-2"
-        onClick={() => launchPad(closedHat)}
+        className={activePad === 'closedHH' ? "box-border h-32 w-32 border-4 rounded-md m-2 border-amber-400" : "box-border h-32 w-32 border-4 rounded-md m-2"}
+        onClick={() => padHandler('closedHH')}
         >
         Closed HH
       </button>
       {/* button below has to change to stop multiple intervals */}
-      <button onClick={() => sequencer()}>Play</button>
-      <button onClick={() => clearInterval(killswitch)}>Stop</button>
+      <button className="p-5 border-4 rounded-md m-2" onClick={() => launchSequencer()}>Play/Stop</button>
+      {/* <button className="p-5 border-4 rounded-md m-2" onClick={() => launchSequencer()}>Stop</button> */}
+      <div className='seq-container grid gap-4 grid-cols-4 grid-rows-4'>
+        {seq.map((b, i) => {
+          return <StepSeqButton index={i} extraCSS={b.extraCSS} key={i} activePad={activePad} step={seq[i]} />
+        })}
+      </div>
     </>
   )
 }
---
+++ src/hooks/useInstruments.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import { audioContext } from "../utils/audioContext"
+
+interface LoadedInstruments {
+  [key: string]: AudioBuffer
+  kick: AudioBuffer
+  clap: AudioBuffer
+  closedHH: AudioBuffer
+}
+
+const getSample = async (audioContext: AudioContext, filepath: string) => {
+  // fetch file
+  const response = await fetch(filepath)
+  // store in arrayBuffer
+  const arrayBuffer = await response.arrayBuffer()
+  //  decode into audioBuffer
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
+  return audioBuffer
+}
+
+// TODO: this can loop over array of samples
+const setSamples = async (filePath: string) => {
+  const sample = await getSample(audioContext, filePath)
+  return sample
+}
+
+const fetchInstruments = async () => {
+  const kick = await setSamples('src/audio/91627__suicidity__dirty-tonys-kick-drum-mx-055.wav')
+  const clap = await setSamples('src/audio/561119__sorinious_genious__clap-9.wav')
+  const closedHH = await setSamples('src/audio/674295__theendofacycle__hi-hat-closed-hit-02.wav')
+  return { kick, clap, closedHH }
+}
+
+const useInstruments = () => {
+  const [instruments, setInstruments]= useState<LoadedInstruments>()
+
+  useEffect(() => {
+    const loadInstruments = async () => {
+      const loadedInstruments = await fetchInstruments()
+      setInstruments(loadedInstruments)
+    }
+    loadInstruments()
+  },[])
+
+  return instruments
+}
+
+export default useInstruments
