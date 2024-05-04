7349b5d1de0a38267c56b5d2e458ed4f8a66d736
dermotbg <dermotbateman@gmail.com>
2024-05-04 15:28:17 +0300

    feat(Fader): touch up UI / fix alignment

--- src/components/MixingDeskContainer/components/FaderContainer/components/Fader/index.tsx
+++ src/components/MixingDeskContainer/components/FaderContainer/components/Fader/index.tsx
@@ -6,8 +6,8 @@ import { AvailableInstruments } from '../../../../../../types'
 
 const Fader = ({ disabled, index, instrument, gain, setGain }: { disabled: boolean, index: number, instrument: AvailableInstruments, gain: number, setGain: (index: number, gain: number, instrument: AvailableInstruments) => void  }) => {
   return(
-    <div className='flex-col'>
-      <div className={"wrapper"}>
+    <div className='flex flex-col items-center'>
+      <div className="wrapper">
         <input 
           type="range"
           id='volume' 
@@ -20,7 +20,7 @@ const Fader = ({ disabled, index, instrument, gain, setGain }: { disabled: boole
           style={disabled ? { backgroundColor: 'rgba(0, 0, 0, .8)' } : undefined }
         />
       </div>
-      <div className='flex flex-col justify-start'>
+      <div className='flex flex-col'>
         {disabled
           ? <FaderDetails instrument={'Inactive'} gain={gain} />
           : <FaderDetails instrument={instrument} gain={gain} />
--- src/components/MixingDeskContainer/components/FaderContainer/components/FaderDetails/index.tsx
+++ src/components/MixingDeskContainer/components/FaderContainer/components/FaderDetails/index.tsx
@@ -6,8 +6,8 @@ type FaderInstrumentTypes = AvailableInstruments | 'Pad' | 'Inactive'
 const FaderDetails = ({ instrument, gain }: { instrument: FaderInstrumentTypes, gain: number }) => {
 
   return(
-    <div className='flex flex-col'> 
-      <div>{instrument}</div> 
+    <div className='flex flex-col '> 
+      <div className="font-mono">{instrument}</div> 
       <div>{((gain / 3.4) * 100).toFixed(0)}%</div>
     </div>
   )
