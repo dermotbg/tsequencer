936137ba8bcc7e535d046f685ae9dc75c4eb5015
dermotbg <dermotbateman@gmail.com>
2024-05-04 13:26:21 +0300

    Feat(StepSeqButton): moved to shadcn component

--- src/components/PadsAndOptionsContainer/index.tsx
+++ src/components/PadsAndOptionsContainer/index.tsx
@@ -4,7 +4,7 @@ import InstrumentPadContainer from "./components/InstrumentPadContainer"
 const PadsAndOptionsContainer = () => {
 
   return(
-    <div className="flex flex-row items-center justify-center m4">
+    <div className="flex flex-row flex-wrap items-center justify-center m4">
       <GlobalOptionsContainer />
       <InstrumentPadContainer />
     </div>
--- src/components/StepSequencerContainer/components/StepSequencer/components/StepSequencerButton/index.tsx
+++ src/components/StepSequencerContainer/components/StepSequencer/components/StepSequencerButton/index.tsx
@@ -1,26 +1,27 @@
+import { Button } from "@/components/ui/button"
 import { StepSeqProps } from "../../../../types"
 
 const StepSeqButton = ({ index, extraCSS, step, onClickHandler }: StepSeqProps) => {
 
   return (
     <div className='flex-col box-border'>
-      <button
-        className={'box-border border-4 p-4 rounded-md w-5/6 h-full min-h-24  ' + `${extraCSS}`}
+      <Button
+        className={'flex-col box-border border-4 p-4 bg-stone-800 rounded-md w-5/6 h-full min-h-24  ' + `${extraCSS}`}
         onClick={() => onClickHandler(index)}
         >
         {index + 1}
-        <div className={!step.instruments.length ? '' : 'flex flex-row justify-apart border-2 border-black rounded-lg'}>
+        <div className={!step.instruments.length ? '' : 'flex flex-row flex-wrap justify-apart border-2 bg-transparent border-black rounded-lg px-3'}>
           {step.instruments.map((i, index) => {
             if(index !== step.instruments.length -1 ){
               return (
-                <div className="border-solid border-r-2 border-black  flex-grow" key={i}>
+                <div className="border-solid border-r-2 border-black flex-grow px-2 text-shadow-sm shadow-black" key={i}>
                   {i[0].toUpperCase()}
                 </div>
               )
             }
             else{
               return (
-                <div className="flex-grow" key={i}>
+                <div className="flex-grow flex-wrap px-2 text-shadow-sm shadow-black" key={i}>
                   {i[0].toUpperCase()}
                 </div>
               )
@@ -28,7 +29,7 @@ const StepSeqButton = ({ index, extraCSS, step, onClickHandler }: StepSeqProps) 
           })}
         </div>
 
-      </button>
+      </Button>
     </div>
   )
 }
