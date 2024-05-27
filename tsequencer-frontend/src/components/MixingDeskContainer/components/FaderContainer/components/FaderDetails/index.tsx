31f5e3aadfe3f32652f34564ceaf5e7e6f4df956
dermotbg <dermotbateman@gmail.com>
2024-05-27 13:16:02 +0300

    Feat(MixingDesk): Control increased instrument size Remove inactive faders in place of null if not in use

--- tsequencer-frontend/src/components/MixingDeskContainer/components/FaderContainer/components/FaderDetails/index.tsx
+++ tsequencer-frontend/src/components/MixingDeskContainer/components/FaderContainer/components/FaderDetails/index.tsx
@@ -1,13 +1,13 @@
 import { AvailableInstruments } from "../../../../../../types"
 
 
-type FaderInstrumentTypes = AvailableInstruments | 'Pad' | 'Inactive'
+type FaderInstrumentTypes = AvailableInstruments | 'Pad' 
 
 const FaderDetails = ({ instrument, gain }: { instrument: FaderInstrumentTypes, gain: number }) => {
 
   return(
-    <div className='flex flex-col '> 
-      <div className="font-mono">{instrument}</div> 
+    <div className='flex flex-col overflow-hidden '> 
+      <div className="font-mono max-w-10 sm:max-w-inherit overflow-hidden ">{instrument.toUpperCase()}</div> 
       <div>{((gain / 3.4) * 100).toFixed(0)}%</div>
     </div>
   )
