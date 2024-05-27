74e0e944d73f30207c030b35a7b29bcc601c1652
dermotbg <dermotbateman@gmail.com>
2024-05-27 13:28:57 +0300

    style(instrumentPad): handle mobile gracefully

--- tsequencer-frontend/src/components/PadsAndOptionsContainer/components/InstrumentPadContainer/components/InstrumentPad/index.tsx
+++ tsequencer-frontend/src/components/PadsAndOptionsContainer/components/InstrumentPadContainer/components/InstrumentPad/index.tsx
@@ -7,17 +7,17 @@ import { Button } from "@/components/ui/button"
 
 const InstrumentPad = ({ instrument, activePad, padHandler}: InstrumentPadType) => {
   return(
-    <>
-        <Button
-          className={setActiveBorder(activePad, instrument) 
-            + '  '}
-          onClick={() => padHandler(validateInstrument(instrument))}
-          >
-            <div className="font-mono text-shadow-sm shadow-black/50 ">
-              {instrument.toUpperCase()}
-            </div>
-        </Button>    
-        </>
+    <div className="">
+      <Button
+        className={setActiveBorder(activePad, instrument) 
+          + ' max-w-20 sm:max-w-none max-h-20 sm:max-h-none '}
+        onClick={() => padHandler(validateInstrument(instrument))}
+        >
+          <div className="font-mono text-shadow-sm shadow-black/50 overflow-scroll sm:overflow-visible ">
+            {instrument.toUpperCase()}
+          </div>
+      </Button>    
+    </div>
   )
 }
 
