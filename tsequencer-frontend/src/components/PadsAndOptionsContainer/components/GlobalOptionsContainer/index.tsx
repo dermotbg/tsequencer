00300d16b5eba5211faaebbdd05aaa444a9d54d1
dermotbg <dermotbateman@gmail.com>
2024-06-05 15:48:11 +0300

    bugfix(keyAssign): Dialog close on submit refactor keyAssignDialog logic to container add errorchecking for duplicate values

--- tsequencer-frontend/src/components/PadsAndOptionsContainer/components/GlobalOptionsContainer/index.tsx
+++ tsequencer-frontend/src/components/PadsAndOptionsContainer/components/GlobalOptionsContainer/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 
+import useAssignedKeysStore from "@/hooks/StateHooks/useAssignedKeysStore";
 import useRecordStore from "@/hooks/StateHooks/useRecordStore";
 import useVolumeStore from "@/hooks/StateHooks/useVolumeStore";
 import useInstruments from "@/hooks/useInstruments";
+import useMessageStore from "@/hooks/StateHooks/useMessageStore";
 import useSequencer from "@/hooks/useSequencer";
+import useWindowSize from "@/hooks/useWindowSize";
 
 import { validateInstrumentRack } from "@/utils/typeChecking";
 import { keyPressHandler } from "./components/KeyTrackController/utils/keyPressHandler";
@@ -12,11 +15,14 @@ import BpmController from "./components/BpmController";
 import KeyTrackController from "./components/KeyTrackController";
 import MetronomeController from "./components/MetromeController";
 import KeyAssignDialog from "./components/KeyAssignDialog";
-import useAssignedKeysStore from "@/hooks/StateHooks/useAssignedKeysStore";
-import useWindowSize from "@/hooks/useWindowSize";
+
+import LoadingSpinner from "@/components/UtilityComponents/LoadingSpinner";
+
+import type { AssignedKeysType } from "@/hooks/StateHooks/useAssignedKeysStore";
 
 const GlobalOptionsContainer = () => {
   const [keysActive, setKeysActive] = useState<boolean>(false);
+  const [isKeyDialogOpen, setIsKeyDialogOpen] = useState<boolean>(false);
 
   const instruments = useInstruments();
   const assignedKeys = useAssignedKeysStore();
@@ -24,6 +30,7 @@ const GlobalOptionsContainer = () => {
   const { level } = useVolumeStore();
   const { record, stepRef } = useRecordStore();
   const windowSize = useWindowSize();
+  const errorMessage = useMessageStore();
 
   const recordingRef = useRef(record);
 
@@ -51,6 +58,34 @@ const GlobalOptionsContainer = () => {
     return () => window.removeEventListener("keydown", keyPressFunction);
   }, [instruments, level, pushToSequencer, stepRef, keysActive, isPlaying]);
 
+  const setKeyTrackingKeyHandler = (i: string, key: string) => {
+    if (!/^[a-zA-Z0-9]$/.test(key)) {
+      errorMessage.set("Keys must be alphanumeric");
+      setTimeout(() => {
+        errorMessage.set(undefined);
+      }, 3000);
+      return;
+    }
+    if (assignedKeys.isDuplicate(key)) {
+      errorMessage.set("Keys must be not be duplicates");
+      setTimeout(() => {
+        errorMessage.set(undefined);
+      }, 3000);
+      return;
+    }
+    assignedKeys.setInputValue(i as keyof AssignedKeysType, key.toUpperCase());
+    assignedKeys.setActiveKey(i as keyof AssignedKeysType, key.toUpperCase());
+  };
+
+  const submitAssignedKeysHandler = () => {
+    setIsKeyDialogOpen(false);
+    assignedKeys.setPrevValue();
+    assignedKeys.setIsDefault(false);
+  };
+
+  if (!instruments) return <LoadingSpinner />;
+  if (!assignedKeys) return <LoadingSpinner />;
+
   return (
     <div className="flex flex-row gap-6">
       <div className="hidden sm:flex flex-col items-start">
@@ -59,7 +94,15 @@ const GlobalOptionsContainer = () => {
           keysActive={keysActive}
           setKeysActive={setKeysActive}
         />
-        <KeyAssignDialog />
+        <KeyAssignDialog
+          instruments={validateInstrumentRack(instruments)}
+          assignedKeys={assignedKeys}
+          isKeyDialogOpen={isKeyDialogOpen}
+          setIsKeyDialogOpen={setIsKeyDialogOpen}
+          setKeyTrackingKeyHandler={setKeyTrackingKeyHandler}
+          submitAssignedKeysHandler={submitAssignedKeysHandler}
+          errorMessage={errorMessage.message}
+        />
       </div>
       <div className="flex flex-col items-start">
         <MetronomeController />
