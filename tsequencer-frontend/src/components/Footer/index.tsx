e939fcd4433723a8d762ffe216b19640eeb3c68c
dermotbg <dermotbateman@gmail.com>
2024-06-11 11:19:22 +0300

    feat(tutorial): add heading color / overflow scroll

--- tsequencer-frontend/src/components/Footer/index.tsx
+++ tsequencer-frontend/src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="py-6 md:px-8 md:py-0 border-t-2">
+    <footer className="mt-1 border-t-2 py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <p className=" text-stone-300 text-balance text-center text-sm ">
+        <p className="text-balance text-center text-sm text-stone-300">
           Built by{" "}
           <a
             href={"https://www.linkedin.com/in/dermot-bateman-7139971a2/"}
