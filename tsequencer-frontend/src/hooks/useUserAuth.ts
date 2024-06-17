bb8de628b401754ad6e44f6863f2b86b705f4246
dermotbg <dermotbateman@gmail.com>
2024-06-17 08:48:51 +0300

    refactor(useUserAuth): combing useLogin $ useRegister hooks

---
+++ tsequencer-frontend/src/hooks/useUserAuth.ts
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+import { loginRequestAsync, logoutRequestAsync } from "@/services/loginService";
+
+import { toast } from "@/components/ui/use-toast";
+
+import useIsLoadingStore from "./StateHooks/useIsLoadingStore";
+import useMessageStore from "./StateHooks/useMessageStore";
+import useUserStore from "./StateHooks/useUserStore";
+
+import type { FormEvent } from "react";
+import { createUserAsync } from "@/services/userService";
+import { prepareSaveUserObject } from "@/components/NavBarContainer/utils/prepareSaveUserObject";
+
+const useUserAuth = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [isRegisterDialogOpen, setIsRegisterDialogOpen] = useState(false);
+  const user = useUserStore();
+  const errorMessage = useMessageStore();
+  const isLoading = useIsLoadingStore();
+
+  const loginHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    isLoading.set(true);
+    try {
+      const resp = await loginRequestAsync({ username, password });
+      // set string to LS only to fire token validation when it's defined
+      localStorage.setItem("user", JSON.stringify("loggedIn"));
+      user.setUsername(resp.username);
+      user.setUserId(resp.id);
+      user.setAuthenticated(true);
+      isLoading.set(false);
+      toast({ description: "You are now logged in." });
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      isLoading.set(false);
+      errorMessage.set(`${error}`.slice(7));
+      setTimeout(() => {
+        errorMessage.set(undefined);
+      }, 10000);
+    }
+  };
+
+  const logoutHandler = () => {
+    user.setUsername(null);
+    user.setUserId(null);
+    user.setAuthenticated(false);
+    localStorage.removeItem("user");
+    logoutRequestAsync();
+  };
+
+  const registerHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await createUserAsync(prepareSaveUserObject(username, password, confPassword));
+      toast({ description: "Registration successful! Please log in." });
+      setIsRegisterDialogOpen(false);
+    } catch (error) {
+      console.log(error);
+      errorMessage.set(`${error}`);
+      setTimeout(() => {
+        errorMessage.set(undefined);
+      }, 5000);
+    }
+  };
+
+  return {
+    username,
+    password,
+    setUsername,
+    setPassword,
+    setConfPassword,
+    isRegisterDialogOpen,
+    setIsRegisterDialogOpen,
+    loginHandler,
+    logoutHandler,
+    registerHandler,
+  };
+};
+
+export default useUserAuth;
