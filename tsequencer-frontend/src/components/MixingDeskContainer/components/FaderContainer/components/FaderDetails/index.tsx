01c7f3e4ec274c4aeb8cd4ccf6ae8720671cdfb9
dermotbg <dermotbateman@gmail.com>
2024-06-18 07:59:13 +0300

    style(FaderDetails): improve font + shortening convention

--- tsequencer-frontend/src/components/MixingDeskContainer/components/FaderContainer/components/FaderDetails/index.tsx
+++ tsequencer-frontend/src/components/MixingDeskContainer/components/FaderContainer/components/FaderDetails/index.tsx
@@ -1,3 +1,5 @@
+import { Volume2 } from "lucide-react";
+
 import type { AvailableInstruments } from "@/types";
 
 type FaderInstrumentTypes = AvailableInstruments | "Pad";
@@ -5,10 +7,16 @@ type FaderInstrumentTypes = AvailableInstruments | "Pad";
 const FaderDetails = ({ instrument, gain }: { instrument: FaderInstrumentTypes; gain: number }) => {
   return (
     <div className="flex flex-col overflow-hidden">
-      <div className="sm:max-w-inherit max-w-10 overflow-hidden font-mono">
-        {instrument.toUpperCase()}
+      <div className="sm:max-w-inherit max-w-10 overflow-hidden border-b-2 font-mono font-semibold">
+        {instrument === "Pad"
+          ? "PAD"
+          : `${instrument.charAt(0).toUpperCase()}` +
+            `${instrument.charAt(instrument.length - 1).toUpperCase()}`}
       </div>
-      <div>{((gain / 3.4) * 100).toFixed(0)}%</div>
+      <div className="flex flex-col items-center pt-1 font-mono font-semibold">
+        <Volume2 size={"16px"} />
+        {((gain / 3.4) * 100).toFixed(0)}%
+      </div>
     </div>
   );
 };
